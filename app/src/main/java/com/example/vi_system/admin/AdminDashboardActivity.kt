07c66e015d627770f518273394874a8a2c3b42da
Alex Yombo <lilkevinrobert@gmail.com>
2023-03-30 11:40:36 -0700

    Added Custom Appbar in admin Dashboard

--- app/src/main/java/com/example/vi_system/admin/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/vi_system/admin/AdminDashboardActivity.kt
@@ -1,15 +1,42 @@
 package com.example.vi_system.admin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.vi_system.R
+import com.google.android.material.appbar.MaterialToolbar
 
 class AdminDashboardActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: MaterialToolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_dashboard)
+
+        toolbar =  findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.top_app_bar,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.signout -> {
+                Toast.makeText(this@AdminDashboardActivity, "Sign Out Clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onBackPressed() {
         AlertDialog.Builder(this)
             .setTitle("Closing the App")
