58e997f87f1e49194e65debc9dd65eb59a070910
Alex Yombo <lilkevinrobert@gmail.com>
2023-04-01 13:24:01 -0700

    update naviagtion drawer to the application

--- app/src/main/java/com/example/vi_system/admin/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/vi_system/admin/AdminDashboardActivity.kt
@@ -4,21 +4,42 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.vi_system.R
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
-class AdminDashboardActivity : AppCompatActivity() {
+class AdminDashboardActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var toolbar: MaterialToolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_dashboard)
 
         toolbar =  findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        navigationView = findViewById(R.id.nav_view)
+        drawerLayout  = findViewById(R.id.drawer_layout)
+
+        val actionBarDrawerToggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            R.string.openNavDrawer,
+            R.string.closeNavDrawer,
+        )
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        navigationView.setNavigationItemSelectedListener(this)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -44,5 +65,9 @@ class AdminDashboardActivity : AppCompatActivity() {
             .setPositiveButton("Yes") { _, _ -> finish() }
             .setNegativeButton("No", null)
             .show()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
