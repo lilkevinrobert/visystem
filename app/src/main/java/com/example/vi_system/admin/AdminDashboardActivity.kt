c981c9ec21c0b8816152c299df125fdcf404eb43
Alex Yombo <lilkevinrobert@gmail.com>
2023-04-05 21:50:09 -0700

    fixed Toolbar issues, was not pulling the navigation drawer

--- app/src/main/java/com/example/vi_system/admin/AdminDashboardActivity.kt
+++ app/src/main/java/com/example/vi_system/admin/AdminDashboardActivity.kt
@@ -11,7 +11,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.vi_system.R
+import com.example.vi_system.admin.fragments.DashboardFragment
+import com.example.vi_system.admin.fragments.LecturerFragment
+import com.example.vi_system.admin.fragments.MaterialFragment
+import com.example.vi_system.admin.fragments.StudentFragment
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
 
@@ -24,9 +29,16 @@ class AdminDashboardActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_dashboard)
+        if (savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view,DashboardFragment::class.java,null)
+                .commit()
+        }
 
         toolbar =  findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        //setSupportActionBar(toolbar)*/
 
         navigationView = findViewById(R.id.nav_view)
         drawerLayout  = findViewById(R.id.drawer_layout)
@@ -34,6 +46,7 @@ class AdminDashboardActivity : AppCompatActivity(),
         val actionBarDrawerToggle: ActionBarDrawerToggle = ActionBarDrawerToggle(
             this,
             drawerLayout,
+            toolbar,
             R.string.openNavDrawer,
             R.string.closeNavDrawer,
         )
@@ -41,7 +54,7 @@ class AdminDashboardActivity : AppCompatActivity(),
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
         navigationView.setNavigationItemSelectedListener(this)
-
+        actionBarDrawerToggle.drawerArrowDrawable.color  = resources.getColor(R.color.white)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -73,22 +86,50 @@ class AdminDashboardActivity : AppCompatActivity(),
         when(item.itemId){
             R.id.dashboard -> {
                 drawerLayout.closeDrawer(GravityCompat.START)
-
+                //item.isChecked = true
+                Toast.makeText(this, "dashboard", Toast.LENGTH_SHORT).show()
+                fragmentReplacement(DashboardFragment())
             }
 
             R.id.lecturers ->{
                 drawerLayout.closeDrawer(GravityCompat.START)
+                fragmentReplacement(LecturerFragment())
             }
 
             R.id.students -> {
                 drawerLayout.closeDrawer(GravityCompat.START)
+                fragmentReplacement(StudentFragment())
             }
             R.id.materials -> {
                 drawerLayout.closeDrawer(GravityCompat.START)
+                fragmentReplacement(MaterialFragment())
+            }
+            R.id.setting ->{
+                drawerLayout.closeDrawer(GravityCompat.START)
+                //navigate to setting ui
+            }
+            R.id.logout -> {
+                drawerLayout.closeDrawer(GravityCompat.START)
+                signOut()
+
             }
 
         }
 
         return true
+    }
+
+
+    private fun fragmentReplacement(fragment: Fragment): Unit {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, fragment)
+            .commit()
+    }
+
+    fun signOut(){
+        //sign out the user to login page
+        Toast.makeText(this, "sign out", Toast.LENGTH_SHORT).show()
+
     }
 }
