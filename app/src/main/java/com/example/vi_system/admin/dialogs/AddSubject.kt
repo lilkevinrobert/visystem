e1fde3133d6a52177f4fb6e05051853495a3c03e
Alex Yombo <lilkevinrobert@gmail.com>
2023-04-10 03:01:16 -0700

    New feature, add new subject dialog

--- app/src/main/java/com/example/vi_system/admin/dialogs/AddSubject.kt
+++ app/src/main/java/com/example/vi_system/admin/dialogs/AddSubject.kt
@@ -1,4 +1,22 @@
 package com.example.vi_system.admin.dialogs
 
-class AddSubject {
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.example.vi_system.R
+
+class AddSubject : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            // Get the layout inflater
+            val inflater = requireActivity().layoutInflater
+            builder.setView(inflater.inflate(R.layout.add_new_subject, null))
+
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
 }
--- app/src/main/java/com/example/vi_system/admin/fragments/SubjectFragment.kt
+++ app/src/main/java/com/example/vi_system/admin/fragments/SubjectFragment.kt
@@ -8,24 +8,22 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vi_system.R
+import com.example.vi_system.admin.dialogs.AddSubject
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
 
 class SubjectFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var addSubjectFab: FloatingActionButton
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_subject, container, false)
         recyclerView = view.findViewById(R.id.recyclerView4)
         addSubjectFab = view.findViewById(R.id.add_subject_fab)
 
         addSubjectFab.setOnClickListener {
-            Toast.makeText(requireContext(), "The sloam church", Toast.LENGTH_SHORT).show()
+            val newFragment = AddSubject()
+            newFragment.show(childFragmentManager, "subject")
         }
         return view
     }
