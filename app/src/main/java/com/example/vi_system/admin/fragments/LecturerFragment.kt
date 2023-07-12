50e728c1c3d1acc1eddebc80da3db6f0093c1aed
Alex Yombo <lilkevinrobert@gmail.com>
2023-07-11 19:34:30 -0700

    Retriving teachers

--- app/src/main/java/com/example/vi_system/admin/fragments/LecturerFragment.kt
+++ app/src/main/java/com/example/vi_system/admin/fragments/LecturerFragment.kt
@@ -8,10 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vi_system.R
 import com.example.vi_system.admin.AddLecturerActivity
+import com.example.vi_system.admin.adpter.StudentAdapter
+import com.example.vi_system.util.Student
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,13 +27,15 @@ private const val ARG_PARAM2 = "param2"
  * Use the [LecturerFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LecturerFragment : Fragment() {
+class LecturerFragment : Fragment(), StudentAdapter.OnStudentClickListener {
 
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var addLecturerFab: FloatingActionButton
     private lateinit var newLecturerFab: FloatingActionButton
     private lateinit var newLecturerTextView: TextView
+    private lateinit var dataList: ArrayList<Student>
+    private lateinit var adapter: StudentAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,6 +50,30 @@ class LecturerFragment : Fragment() {
         addLecturerFab =  view.findViewById(R.id.add_lecturer_fab)
         newLecturerFab = view.findViewById(R.id.add_new_lecturer)
         newLecturerTextView = view.findViewById(R.id.add_lecturer_text)
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL,false)
+        dataList = ArrayList()
+        adapter = StudentAdapter(requireContext(), dataList,this)
+        recyclerView.adapter = adapter
+
+        val databaseReference = FirebaseDatabase.getInstance().getReference("users")
+
+        val query: Query = databaseReference.orderByChild("role").equalTo("lecturer")
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot in dataSnapshot.children) {
+                    // Retrieve the student data
+                    val student = snapshot.getValue(Student::class.java)
+                    dataList.add(student!!)
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(requireActivity(), databaseError.message.toString(), Toast.LENGTH_LONG).show()
+            }
+        })
 
         newLecturerFab.visibility = View.GONE
         newLecturerTextView.visibility = View.GONE
@@ -70,6 +100,10 @@ class LecturerFragment : Fragment() {
 
         return view
 
+    }
+
+    override fun onStudentClickListener(position: Int) {
+        val user: Student = dataList[position]
     }
 
 }
